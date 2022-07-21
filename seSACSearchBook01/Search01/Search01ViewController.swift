94940f9283604caf62e4330e8ad96b8ab4726ac6
peisns <hansol1501@gmail.com>
2022-07-21 23:04:06 +0900

    failed to nav screen transition

---
+++ seSACSearchBook01/Search01/Search01ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Search01ViewController.swift
+//  seSACSearchBook01
+//
+//  Created by Brother Model on 2022/07/21.
+//
+
+import UIKit
+
+class Search01ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- seSACSearchBook01/ViewController.swift
+++ seSACSearchBook01/ViewController.swift
@@ -14,6 +14,16 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func naviSearchButtonClicked(_ sender: UIBarButtonItem) {
+        let sb = UIStoryboard(name: "Search01", bundle: nil)
+        let vc = sb.instantiateViewController(withIdentifier: "Search01ViewController") as! Search01ViewController
+        
+        let nav = UINavigationController(rootViewController: vc)
+        
+        nav.modalPresentationStyle = .fullScreen
+        navigationController?.pushViewController(nav, animated: true) // 1번 코드
+//        present(nav, animated: true, completion: nil) // 2번코드
+    }
+    
 }
 
